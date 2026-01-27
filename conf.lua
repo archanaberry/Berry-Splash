b6d4cb2095294cdde0a5a5d929425640f39270e5
ka-shifuka <aryadewanggapuja@gmail.com>
2026-01-27 12:05:12 +0700

    Add structure folder to work with

---
+++ conf.lua
@@ -0,0 +1,15 @@
+---@diagnostic disable-next-line : duplicate-set-field
+love.conf = function(T)
+	T.identity = "Berry Splash"
+
+	--- gunakan versi ini ya sayang
+	T.version = "11.5"
+
+	T.window.title = "Berry Splash"
+	--- untuk membuatnya berorientasi landscape
+	T.window.width = 1200
+	T.window.height = 600
+	T.window.fullscreen = true
+	T.window.usedpiscale = true
+
+end
